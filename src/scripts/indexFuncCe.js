b4c1a4a480c405ed1c3d160828b5b20c9161ae06
David Hernández <david.hernandezc@iteso.mx>
2024-03-27 14:30:32 -0600

    functional menu

--- src/scripts/indexFuncCe.js
+++ src/scripts/indexFuncCe.js
@@ -1,34 +1,10 @@
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    adjustHeightToViewport();
-
-    window.addEventListener("resize", function() {
-        adjustHeightToViewport();
-    });
-});
-
-function adjustHeightToViewport() {
-    // Lista de selectores de los elementos que deseas ajustar.
-    var selectors = ['.background-iphone', '.diagonal-lines-iphone', '.rectangle-iphone'];
-
-    // Altura de la ventana del navegador
-    var windowHeight = window.innerHeight + 'px';
-
-    // Ajustar la altura de cada elemento seleccionado.
-    selectors.forEach(function(selector) {
-        var elements = document.querySelectorAll(selector);
-        elements.forEach(function(element) {
-            element.style.height = windowHeight;
-        });
-    });
-}
-*/
-
 document.addEventListener('DOMContentLoaded', function() {
     const botonMenu = document.querySelector('.menu'); // Selecciona el botón del menú
-    const menu = document.querySelector('.menu-abierto'); // Selecciona el menú
+    const menu = document.querySelector('.menu-abierto');
 
     botonMenu.addEventListener('click', function() {
-        menu.classList.toggle('mostrar'); // Alterna la clase para mostrar u ocultar el menú
+        menu.classList.toggle('mostrar');
     });
+
+
 });
