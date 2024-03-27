aa2ea9a7ca5bb316395d166acba3f58592c980ce
David Hernández <david.hernandezc@iteso.mx>
2024-03-27 00:37:11 -0600

    indexHeight adjust

---
+++ src/scripts/indexFuncCe.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    adjustHeightToViewport();
+
+    window.addEventListener("resize", function() {
+        adjustHeightToViewport();
+    });
+});
+
+function adjustHeightToViewport() {
+    // Lista de selectores de los elementos que deseas ajustar.
+    var selectors = ['.background-iphone', '.diagonal-lines-iphone', '.rectangle-iphone'];
+
+    // Altura de la ventana del navegador
+    var windowHeight = window.innerHeight + 'px';
+
+    // Ajustar la altura de cada elemento seleccionado.
+    selectors.forEach(function(selector) {
+        var elements = document.querySelectorAll(selector);
+        elements.forEach(function(element) {
+            element.style.height = windowHeight;
+        });
+    });
+}
