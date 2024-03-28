9fe6ab083f0e03844833b8edbcae7ae32b1e2369
David Hernández <david.hernandezc@iteso.mx>
2024-03-27 18:55:42 -0600

    .

--- src/scripts/indexFuncCe.js
+++ src/scripts/indexFuncCe.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const botonMenu = document.querySelector('.menu'); // Selecciona el botón del menú
     const menu = document.querySelector('.menu-abierto');
+    const closeMenu = document.querySelector('.close')
 
     botonMenu.addEventListener('click', function() {
         menu.classList.toggle('mostrar');
     });
 
-
+    closeMenu.addEventListener('click', () =>{
+        menu.classList.toggle('mostrar');
+    })
 });
 
 function dat ()
