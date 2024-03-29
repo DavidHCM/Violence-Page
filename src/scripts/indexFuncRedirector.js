e593b015505f1f189120c4cb60746c1ded674c7c
David Hernández <david.hernandezc@iteso.mx>
2024-03-28 18:01:35 -0600

    .

--- src/scripts/indexFuncRedirector.js
+++ src/scripts/indexFuncRedirector.js
@@ -1,6 +1,6 @@
 
 function detectarDispositivo() {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         window.location.href = "/Violence-Page/src/views/indexCe.html";
     } else {
         window.location.href = "/Violence-Page/src/views/indexCo.html";
